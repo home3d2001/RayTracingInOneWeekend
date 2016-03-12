b5f2741aa2198aa4a9e2b222f9c6aac827f92037
jnadro <jason.nadro@gmail.com>
2016-03-12 09:38:34 -0600

    Chapter 7 - Ray Tracer In One Weekend
    - Added Diffuse material
    - random sampling of a sphere
    - gamma correction

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 #include <limits>
 #include <random>
@@ -5,10 +6,34 @@
 #include "sphere.h"
 #include "hitableList.h"
 
-vec3 color(const Ray& r, Hitable* world) {
+struct UniformRandomSampler {
+	UniformRandomSampler()
+		: gen(std::random_device()())
+	{
+	}
+
+	float getNextSample() {
+		std::uniform_real_distribution<float> dis(0.0f, 1.0f);
+		return (float)dis(gen);
+	}
+
+	std::mt19937 gen;
+};
+
+// easiest algorithm: rejection method
+vec3 randomInUnitSphere(UniformRandomSampler& sampler) {
+	vec3 p;
+	do {
+		p = 2.0f * vec3(sampler.getNextSample(), sampler.getNextSample(), sampler.getNextSample()) - vec3(1.0f, 1.0f, 1.0f);
+	} while (dot(p, p) >= 1.0);
+	return p;
+}
+
+vec3 color(const Ray& r, Hitable* world, UniformRandomSampler& sampler) {
 	hitRecord rec;
 	if (world->hit(r, 0.0f, std::numeric_limits<float>::max(), rec)) {
-		return 0.5f * (rec.normal + vec3(1.0f, 1.0f, 1.0f));
+		const vec3 target = rec.p + rec.normal + randomInUnitSphere(sampler);
+		return 0.5f * color(Ray(rec.p, target - rec.p), world, sampler); // (rec.normal + vec3(1.0f, 1.0f, 1.0f));
 	}
 	else {
 		vec3 unitDirection = makeUnitVector(r.direction());
@@ -18,13 +43,9 @@ vec3 color(const Ray& r, Hitable* world) {
 }
 
 int main() {
-	const int nx = 200;
-	const int ny = 100;
+	const int nx = 600;
+	const int ny = 300;
 	const int ns = 100;
-
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_real_distribution<> dis(0.0f, 1.0f);
 
 	std::cout << "P3\n" 
 			  << nx << " " << ny 
@@ -41,19 +62,24 @@ int main() {
 
 	Hitable* world = new HitableList(list, 2);
 
+	UniformRandomSampler sampler;
+
 	Camera camera;
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
 			vec3 col(0.0f, 0.0f, 0.0f);
 
 			for (int s = 0; s < ns; ++s) {
-				const float u = float(i + dis(gen)) / float(nx);
-				const float v = float(j + dis(gen)) / float(ny);
+				const float u = float(i + sampler.getNextSample()) / float(nx);
+				const float v = float(j + sampler.getNextSample()) / float(ny);
 				const Ray r = camera.getRay(u, v);
-				col += color(r, world);
+				col += color(r, world, sampler);
 			}
 
 			col /= float(ns);
+
+			constexpr float gamma = 1.0f / 2.2f;
+			col = vec3(std::pow(col.r(), gamma), std::pow(col.g(), gamma), std::pow(col.b(), gamma));
 
 			const int ir = int(255.99 * col.r());
 			const int ig = int(255.99 * col.g());
