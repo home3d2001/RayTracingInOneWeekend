732117128f63f2c6a0bbfd473bc23fe4984bb069
jnadro <jason.nadro@gmail.com>
2016-03-20 14:04:19 -0500

    Chapter 3 - GPU Ray Tracing In One Weekend
    - Adding cutil_math.h for float3 operations from
    http://www.icmc.usp.br/~castelo/CUDA/common/inc/cutil_math.h
    - Adding glfw binaries for visual studio 2013 64 bit

--- src/kernel.cu
+++ src/kernel.cu
@@ -1,10 +1,50 @@
-
 #include "cuda_runtime.h"
 #include "curand_kernel.h"
+#include "cutil_math.h"
 #include "device_launch_parameters.h"
 
 #include <assert.h>
 #include <stdio.h>
+
+#include <GLFW/glfw3.h>
+
+#define M_PI 3.14159265359f
+
+struct Ray { 
+	float3 o;
+	float3 d;
+	__device__ Ray(float3 a, float3 b) : o(a), d(b) {} 
+};
+
+__device__ float3 color(Ray& r) {
+	float3 unitDirection = normalize(r.d);
+	float t = 0.5f * (unitDirection.y + 1.0f);
+	float3 white = make_float3(1.0f, 1.0f, 1.0f);
+	float3 blue = make_float3(0.5f, 0.7f, 1.0f);
+	return (1.0f - t) * white + t * blue;
+}
+
+__global__ void render(float3* pixels, const unsigned int sizeX, const unsigned int sizeY) {
+	 unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;   
+	 unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+	 unsigned int idx = y * sizeX + x;
+
+	 curandState randState;
+	 curand_init(idx, 0, 0, &randState);
+
+	 float3 lowerLeftCorner = make_float3(-2.0f, -1.0f, -1.0f);
+	 float3 horizontal = make_float3(4.0f, 0.0f, 0.0f);
+	 float3 vertical = make_float3(0.0f, 2.0f, 0.0f);
+	 float3 origin = make_float3(0.0f, 0.0f, 0.0f);
+
+	 float u = x / (float)sizeX;
+	 float v = 1 - y / (float)sizeY;
+
+	 Ray r(origin, lowerLeftCorner + u * horizontal + v * vertical);
+
+	 pixels[idx] = color(r);
+}
 
 __global__ void clearPixels(float3* pixels, const unsigned int sizeX, const unsigned int sizeY) {
 	 unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;   
@@ -44,7 +84,7 @@ cudaError_t clearImageWithCuda(float3 color, float3* hostPixels, unsigned int si
 
 	dim3 block(16, 16, 1);
 	dim3 grid(sizeX / block.x, sizeY / block.y, 1);
-	randomPixels<<<grid, block>>>(devicePixels, sizeX, sizeY);
+	render<<<grid, block>>>(devicePixels, sizeX, sizeY);
 	VERIFY_CUDA(cudaGetLastError());
 	VERIFY_CUDA(cudaDeviceSynchronize());
 	VERIFY_CUDA(cudaMemcpy(hostPixels, devicePixels, byteCount, cudaMemcpyDeviceToHost));
@@ -59,6 +99,9 @@ int main()
 	const int nx = 1280;
 	const int ny = 720;
 
+    if (!glfwInit())
+        return -1;
+
 	float3* pixels = new float3[nx * ny];
 
 	cudaError_t cudaStatus = clearImageWithCuda(make_float3(1.0f, 0.0f, 1.0f), pixels, nx, ny);
@@ -67,7 +110,7 @@ int main()
         return 1;
 	}
 
-	FILE* f = fopen( "image.ppm", "w" );
+	FILE* f = fopen( "gpu.ppm", "w" );
 	fprintf(f, "P3\n%d %d\n%d\n", nx, ny, 255);
 	for (int i = 0; i < nx * ny; ++i) {
 		fprintf(f, "%d %d %d ", int(255.99 * pixels[i].x),
